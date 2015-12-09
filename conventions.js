ee027442624f4f11b3f1f6ae968efba51876e3e4
Gabriel Florit <gabrielflorit@gmail.com>
2015-12-09 17:29:23 -0500

    Add code

---
+++ conventions.js
@@ -0,0 +1,38 @@
+import d3 from 'd3';
+
+export default function conventions(opt) {
+
+	let options = opt || {};
+
+	const margin = options.margin || {top: 1, right: 1, bottom: 1, left: 1};
+	const outerWidth = options.width || 800;
+	const outerHeight = options.height || 500;
+
+	const width = outerWidth - margin.left - margin.right;
+	const height = outerHeight - margin.top - margin.bottom;
+
+	const parentSelection = options.parentSelection || d3.select('body');
+
+	const svg = options.svg || parentSelection.append('svg').attr({
+		outerWidth,
+		outerHeight
+	}).append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+	const x = options.x || d3.scale.linear().range([0, width]);
+	const y = options.y || d3.scale.linear().range([height, 0]);
+
+	const xAxis = d3.svg.axis().scale(x).orient('bottom');
+	const yAxis = d3.svg.axis().scale(y).orient('left');
+
+	return {
+		width,
+		height,
+		svg,
+		x,
+		y,
+		xAxis,
+		yAxis
+	};
+
+}
+
